3bec0969e25e2620b90c8091b9cfc57e765f7b8e
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2018-11-15 17:22:24 -0200

    First commit

---
+++ 04 Comments/09_comments_single_line/comments.py
@@ -0,0 +1,2 @@
+# this is a single line comment
+print('Working with single line comments (#).') # this another single line comment
---
+++ 04 Comments/10_comments_multiple_lines/comments.py
@@ -0,0 +1,5 @@
+'''
+    A bunch of comments!
+    Can you see?
+'''
+print('Working with comments part 2!')
