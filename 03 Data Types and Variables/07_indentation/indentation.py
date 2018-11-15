71757c6c6961154f0c406225569da0a0e3fd6a3c
Antonio Henrique de Melo Costa <ahmcosta@gmail.com>
2018-11-15 14:59:02 -0200

    Initial commit

---
+++ 03 Data Types and Variables/07_indentation/indentation.py
@@ -0,0 +1,14 @@
+def test():
+    print('test - statement1')
+    print('test - statement2')
+    print('test - statement3')
+
+test()
+print('this isn\'t a test\'s statement')
+
+
+def test2():
+    print('test2 - statement1')
+    print('test2 - statement2')
+
+test2()
